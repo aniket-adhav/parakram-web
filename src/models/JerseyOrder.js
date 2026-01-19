599f12a47edb398d2c869ab5d5ae7e8ea65b8e02
aniketadhav55 <aniketadhav2006@gmail.com>
2026-01-20 01:46:20 +0530

    Updated Code

--- src/models/JerseyOrder.js
+++ src/models/JerseyOrder.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const JerseyOrderSchema = new mongoose.Schema({
   name: { type: String, required: true },
 
-  email: { type: String, required: true, unique: true },
+  email: { type: String, unique: true },
 
   phone: {
     type: String,
