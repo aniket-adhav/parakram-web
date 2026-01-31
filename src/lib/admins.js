91c0d4b67ca3a8c71977d56930329f3b29b61589
aniketadhav55 <aniketadhav2006@gmail.com>
2026-01-31 10:37:32 +0530

    Fix admin auth import and order verification

--- src/lib/admins.js
+++ src/lib/admins.js
@@ -2,7 +2,8 @@
 
 export const MASTER_ADMINS = [
   "aniketadhav022@gmail.com",
-  "viveknikam22104@gmail.com"
+  "viveknikam22104@gmail.com",
+  "p07164753@gmail.com"
 ];
 
 export const DEPARTMENT_ADMINS = {
